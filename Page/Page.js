ffc20d384e3c806996e86c7cafcd52ddd25398c3
Dennis Lessa <dennislessa@hotmail.com>
2020-01-18 23:33:50 -0300

    Atualização

--- Page/Page.js
+++ Page/Page.js
@@ -1,86 +1,63 @@
-/**
- * @name Page
- * @description File that handle paginators.
- * 
- * Classes:
- * 
- * page-prev    Close current page and go back previous page.
- * page-next    Open next page indicate on href link.
- */
-(function( doc ){
+(function(global){
     'use strict';
 
-    var links,
-        page_stack;
-
-    /**
-     * Function that initialize variables.
-     */
-    function init() {
-        links = doc.querySelectorAll( '.page-prev, .page-next' );
-        page_stack = [];
-
-        click_on_link();
-    }
-
-    /**
-     * Function that run an action on click on link.
-     */
-    function click_on_link() {
-        if (links.length > 0) {
-            links.forEach(function(link) {
-                link.onclick = function() {
-                    if (this.className.indexOf('page-next') > -1) {
-                        get_next_page( this );
-                    } else if (this.className.indexOf('page-prev') > -1) {
-                        get_back_page( this );
-                    } else {
-                        console.warn('It is a any link.');
-                    }
-
-                    return false;
-                };
-            });
-        }
-    }
-
-    /**
-     * Function that open the next page.
-     * 
-     * @param {Node} link Element HTML <a>
-     */
-    function get_next_page( link ) {
-        var current_page = link.closest('.page'),
-            next_page = link.href ? link.href.replace(/^(.*)#/, ''): null;
-        
-        if (current_page && next_page) {
-            page_stack.push(current_page.getAttribute('data-page'));
-
-            current_page.classList.add('open-overlay');
-            
-            doc.querySelector(`[data-page="${next_page}"]`).classList.add('active');
-        } else {
-            console.warn('Current page or next page not exists.');
-        }
-    }
-
-    /**
-     * Function that close the current page and go back previous page.
-     * 
-     * @param {Node} link Element HTML <a>
-     */
-    function get_back_page( link ) {
-        var current_page = link.closest('.page'),
-            prev_page = page_stack.pop();
-        
-        if (current_page && prev_page) {
-            doc.querySelector(`[data-page="${prev_page}"]`).classList.remove('open-overlay');
+    var count = 0;
     
-            current_page.classList.remove('active');
-        } else {
-            console.warn('Current page or previous page not exists.');
-        }
+    function Page( id ) {
+        this.element = document.querySelector(`[data-page="${id}"]`);
+        this.links = document.querySelectorAll(`[href="#${id}"]`);
     }
 
-    doc.addEventListener('DOMContentLoaded', init);
-})(document);
+    Page.prototype = {
+        onload: function(callback) {
+            var self = this;
+
+            if (count == 0) {
+                count++;
+
+                global.addEventListener('DOMContentLoaded', function(){
+                    if (self.element && self.element.className.indexOf('active') > -1) {
+                        callback();
+                    }
+                });
+            } else {
+                throw 'Load event already exists.';
+            }
+
+            return this;
+        },
+
+        request: function(callback) {
+            if (this.links) {
+                this.links.forEach(function(link) {
+                    link.addEventListener('click', function() {
+                        if (typeof callback === 'function') {
+                            callback();
+                        }
+
+                        return false;
+                    });
+                });
+            }
+        }
+    };
+
+    global.Page = Page;
+})(window);
+
+var intro = new Page('intro');
+var superservices = new Page('superservices');
+
+intro.onload(function() {
+    // Put here code for intro page on load page
+});
+
+intro.request(function(){
+    // Put here code for intro page on click link
+    console.info('Voce clicou no link da pagina INTRO.');
+});
+
+superservices.request(function(){
+    // Put here code for intro page on click link
+    console.info('Voce clicou no link da pagina SUPERSERVIÇOS.');
+})
